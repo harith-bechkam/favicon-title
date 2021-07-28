9f96df95a13a6c3928311ca4e8c51644bdb1f7d1
harith-beckham <harith.kv47@gmail.com>
2021-07-28 15:56:10 +0530

    updated v2

--- pages/index.js
+++ pages/index.js
@@ -2,43 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {useState,useEffect} from 'react'
-import { NextSeo } from 'next-seo';
-// import Head from './../node_modules/next/head'
 
-
- const Home=()=> {
-
-const [img,setImage]=useState("")
-  
-const [text,setText]=useState("")
-  
-    useEffect(()=>{
-async function fetchData(){
-const response=await fetch('https://testingapi.smartdiner.co/before_login/restaurant/get_full_details')
-const data=await response.json()
-
-// console.log(data.restaurant.logo)
-// console.log(data.restaurant.name)
-
-setImage(data.restaurant.logo)
-setText(data.restaurant.name)
-}
-fetchData()
-    },[])
-  
-
-
+ const Home=({results})=> {
 
   return(
 <div>
-
-
-        <Head>
-          <link rel="shortcut icon" href={img} />
-          <title>{text}</title>
+         
+          <Head>
+          <link rel="shortcut icon" href={results.restaurant.logo} />
+          <title>{results.restaurant.name}</title>
         </Head>
+        
 </div>
     )
+}
+
+export async function getServerSideProps(context){
+ const options={ headers: { Origin: 'http://localhost:3000' } }
+  const res=await fetch(`https://testingapi.smartdiner.co/before_login/restaurant/get_full_details`,options)
+  const json=await res.json()
+  return{
+    props:{
+      results:json
+    },
+  }
 }
 
 
